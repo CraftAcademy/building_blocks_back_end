91bf46180c7122cbf1dfabfcb2797c1c28eebc1f
felixottander <felix.ottander@yahoo.com>
2017-04-20 21:04:47 +0200

    Adds rspec for admin and factory.

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+ describe 'DB table' do
+   it { is_expected.to have_db_column :email }
+   it { is_expected.to have_db_column :encrypted_password }
+ end
+
+ describe 'Validations' do
+   it { is_expected.to validate_presence_of :email }
+   it { is_expected.to have_db_column :encrypted_password }
+ end
+
+ describe 'Factory' do
+   it 'has a valid factory' do
+     expect(create(:admin)).to be_valid
+   end
+ end
 end

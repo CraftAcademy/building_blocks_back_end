0f07f78dba703d9fe8041f30e302526b8d1b0207
casperjureskog <lcasper@mac.com>
2017-04-23 11:39:18 +0200

    add feature test for show workorders

--- features/step_definitions/help_request_steps.rb
+++ features/step_definitions/help_request_steps.rb
@@ -7,3 +7,17 @@ end
 Given(/^I am on the show help request page$/) do
   visit help_requests_path
 end
+
+Given(/^the following work orders messages exist$/) do |table|
+  table.hashes.each do |hash|
+    create(:workorder, title: hash[:title], message: hash[:message], location: hash[:location], urgent: hash[:urgent])
+  end
+end
+
+Given(/^I am on the work orders index page$/) do
+  visit workorders_path
+end
+
+Then(/^I select "([^"]*)" from "([^"]*)"$/) do |option, category|
+  select option, from: category
+end

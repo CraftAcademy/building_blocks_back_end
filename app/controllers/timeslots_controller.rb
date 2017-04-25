dfaf821289348b45c1b0a88adb783f528d773085
casperjureskog <lcasper@mac.com>
2017-04-26 00:09:47 +0200

    add corections to controller calculations

--- app/controllers/timeslots_controller.rb
+++ app/controllers/timeslots_controller.rb
@@ -1,16 +1,29 @@
 class TimeslotsController < ApplicationController
   def create
-    binding.pry
-    @time = params[:start_time]
-    params[:time_slot].times do
-      binding.pry
-      if @time + 4 > 24
-      newt = (@time + 4)- 24
-        @time = 0 + newt
+    time = params[:start_time].to_i
+    slot = params[:time_slot].to_i
+    newt = 0
+    slot.times do |slot|
+      if time > 9
+        timeslot = Timeslot.new(start_time: "#{time}:00:00", facility_id: params[:facility_id])
+      else
+        timeslot = Timeslot.new(start_time: "0#{time}:00:00", facility_id: params[:facility_id])
       end
-      Timeslot.new(start_time: @time)
-      @time + 4
-      Timeslot.new(start_time: @time)
+      if time + slot == 24
+        time = 0
+      elsif time + slot > 24
+        newt += ((time + slot) - 24)
+        time = (0 + newt)
+      else
+        time += 4
+      end
+      binding.pry
+      if time > 9
+        timeslot.update(end_time: "#{time}:00:00")
+      else
+        timeslot.update(end_time: "0#{time}:00:00")
+      end
+      timeslot.save
     end
   end
 end

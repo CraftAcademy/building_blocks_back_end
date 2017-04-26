d5f8baec824aa534f9448c9ae2b11d5509f70371
casperjureskog <lcasper@mac.com>
2017-04-26 11:43:18 +0200

    add small fix

--- app/models/timeslot.rb
+++ app/models/timeslot.rb
@@ -7,19 +7,19 @@ class Timeslot < ApplicationRecord
     slot_times = params[:time_slot].to_i
     newt = 0
     slot_times.times do
-      slot = params[:time_slot].to_i
+      hours = params[:hours].to_i
       if time > 9
         timeslot = Timeslot.new(start_time: "#{time}:00:00", facility_id: params[:facility_id])
       else
         timeslot = Timeslot.new(start_time: "0#{time}:00:00", facility_id: params[:facility_id])
       end
-      if time + slot === 24
+      if time + hours === 24
         time = 0
-      elsif time + slot > 24
-        newt += ((time + slot) - 24)
+      elsif time + hours > 24
+        newt += ((time + hours) - 24)
         time = (0 + newt)
       else
-        time += slot
+        time += hours
       end
       if time > 9
         timeslot.update(end_time: "#{time}:00:00")
--- features/booking_admin.feature
+++ features/booking_admin.feature
@@ -9,8 +9,8 @@ Feature: Admin booking page
     And I click link "Laundry Room"
     Then I should see "scheduleing controll for Laundry Room"
     And I select "9" from "start_time"
-    And I select "4" from "time_slot"
-    And I select "4" from "hours"
+    And I select "5" from "time_slot"
+    And I select "3" from "hours"
     And I click "Set schedule"
     Then I should see "schedule created"
     And I am on the facility show page

6a558a9616f6a55549ae5dd9a34051c087bbdcbb
casperjureskog <lcasper@mac.com>
2017-05-03 17:18:27 +0200

    add fix for booking mobile

--- app/controllers/api/v1/timeslots_controller.rb
+++ app/controllers/api/v1/timeslots_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::TimeslotsController < ApiController
   def index
     @facility = Facility.find(params[:facility_id])
+    @bookings = Booking.where(facility_id: params[:facility_id])
+    date = params[:date]
+    @date =(date.to_time + 2.hours).strftime("%F")
   end
 end

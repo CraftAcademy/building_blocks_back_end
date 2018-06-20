f56ad3d08a2b35d99219eac9686021fd06ec3bab
casperjureskog <lcasper@mac.com>
2018-06-20 14:37:56 +0200

    removes a pry

--- app/controllers/help_requests_controller.rb
+++ app/controllers/help_requests_controller.rb
@@ -5,7 +5,6 @@ class HelpRequestsController < ApplicationController
 
   def destroy
     @help_request = HelpRequest.find(params[:id])
-    binding.pry
     if @help_request.destroy
       flash[:notice] = "Help Request Deleted!"
       redirect_back(fallback_location: building_help_requests_path(session[:current_building_id]))
